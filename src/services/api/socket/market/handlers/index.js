914c1fdbbcd7d90e7570de054dc7a1f6973c9a15
Mateusz Krzyżanowski <matkrzy94@gmail.com>
2018-12-04 09:08:56 +0100

    Market updates

--- src/services/api/socket/market/handlers/index.js
+++ src/services/api/socket/market/handlers/index.js
@@ -7,7 +7,7 @@ const { handleGetLocalMarket } = require('./handleGetLocalMarket');
 
 const marketEvents = (connection, client) => {
   client.on(MARKET_GET, handleGetMarket);
-  client.on(MARKET_UPDATE, handleUpdateMarket(connection));
+  client.on(MARKET_UPDATE, handleUpdateMarket(connection, client));
   client.on(MARKET_ADD_GOODS, handleAddNewGoods(client));
 
   client.on(MARKET_LOCAL_GET, handleGetLocalMarket);
